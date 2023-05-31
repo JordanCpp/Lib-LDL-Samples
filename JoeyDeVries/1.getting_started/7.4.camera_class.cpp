ceb5987c9db9fb24625c18fdc48f41778c02139a
Евгений <jordan631@yandex.ru>
2023-05-31 18:04:07 +0400

    Refactoring

---
+++ JoeyDeVries/1.getting_started/7.4.camera_class.cpp
@@ -0,0 +1,269 @@
+#include <iostream>
+#include <LDL/Graphics/Window.hpp>
+#include <LDL/Graphics/Render.hpp>
+#include <LDL/Time/Ticks.hpp>
+#include <LDL/Allocators/FixedLinear.hpp>
+#include <LDL/Loaders/ImageLoader.hpp>
+#include <LDL/Core/RuntimeError.hpp>
+#include <LDL/OpenGL/OpenGL3_3.hpp>
+
+#include <learnopengl/shader_m.h> 
+#include <learnopengl/texture.h>
+#include <learnopengl/camera.h>
+
+using namespace LDL::Graphics;
+using namespace LDL::Events;
+using namespace LDL::Enums;
+using namespace LDL::Core;
+using namespace LDL::Allocators;
+using namespace LDL::Loaders;
+using namespace LDL::Math;
+using namespace LDL::Time;
+
+void processInput(Event& report);
+void mouse_callback(Event& report);
+
+// settings
+const unsigned int SCR_WIDTH  = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
+int main()
+{
+    try
+    {
+        Window window(Vec2u(0, 0), Vec2u(SCR_WIDTH, SCR_HEIGHT), "LearnOpenGL");
+
+        RenderContext renderContext;
+        Render render(&renderContext, &window);
+
+        FixedLinear allocator(Allocator::Mb * 8);
+        ImageLoader loader(&allocator);
+
+        Event report;
+
+        // configure global opengl state
+        // -----------------------------
+        glEnable(GL_DEPTH_TEST);
+
+        // build and compile our shader zprogram
+        // ------------------------------------
+        Shader ourShader("resources/shaders/7.4.camera.vs", "resources/shaders/7.4.camera.fs");
+
+        // set up vertex data (and buffer(s)) and configure vertex attributes
+        // ------------------------------------------------------------------
+        float vertices[] = {
+            -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+             0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+             0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+            -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+             0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+             0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+             0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+             0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+             0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+             0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+             0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+        };
+
+        // world space positions of our cubes
+        glm::vec3 cubePositions[] = {
+            glm::vec3(0.0f,  0.0f,  0.0f),
+            glm::vec3(2.0f,  5.0f, -15.0f),
+            glm::vec3(-1.5f, -2.2f, -2.5f),
+            glm::vec3(-3.8f, -2.0f, -12.3f),
+            glm::vec3(2.4f, -0.4f, -3.5f),
+            glm::vec3(-1.7f,  3.0f, -7.5f),
+            glm::vec3(1.3f, -2.0f, -2.5f),
+            glm::vec3(1.5f,  2.0f, -2.5f),
+            glm::vec3(1.5f,  0.2f, -1.5f),
+            glm::vec3(-1.3f,  1.0f, -1.5f)
+        };
+
+        unsigned int VBO, VAO;
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+
+        glBindVertexArray(VAO);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        // position attribute
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+        // texture coord attribute
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+
+        // load and create a texture 
+        // -------------------------
+        GLuint texture1 = LoadTexture("resources/textures/container.jpg", loader);
+        GLuint texture2 = LoadTexture("resources/textures/awesomeface.png", loader);
+
+        // tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
+        // -------------------------------------------------------------------------------------------
+        ourShader.use();
+        ourShader.setInt("texture1", 0);
+        ourShader.setInt("texture2", 1);
+
+        while (window.Running())
+        {
+            deltaTime = 0.1f;
+
+            while (window.GetEvent(report))
+            {
+                if (report.Type == IsResize)
+                {
+                    glViewport(0, 0, (GLsizei)report.Resize.Width, (GLsizei)report.Resize.Height);
+                }
+
+                if (report.Type == IsQuit || report.IsKeyPressed(KeyboardKey::Escape))
+                {
+                    window.StopEvent();
+                }
+
+                mouse_callback(report);
+                processInput(report);
+            }
+
+            render.Begin();
+
+            // render
+            // ------
+            glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            // bind textures on corresponding texture units
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, texture1);
+            glActiveTexture(GL_TEXTURE1);
+            glBindTexture(GL_TEXTURE_2D, texture2);
+
+            // activate shader
+            ourShader.use();
+
+            // pass projection matrix to shader (note that in this case it could change every frame)
+            glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+            ourShader.setMat4("projection", projection);
+
+            // camera/view transformation
+            glm::mat4 view = camera.GetViewMatrix();
+            ourShader.setMat4("view", view);
+
+            // render boxes
+            glBindVertexArray(VAO);
+            for (unsigned int i = 0; i < 10; i++)
+            {
+                // calculate the model matrix for each object and pass it to shader before drawing
+                glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+                model = glm::translate(model, cubePositions[i]);
+                float angle = 20.0f * i;
+                model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+                ourShader.setMat4("model", model);
+
+                glDrawArrays(GL_TRIANGLES, 0, 36);
+            }
+
+            render.End();
+
+            window.PollEvents();
+        }
+
+        // optional: de-allocate all resources once they've outlived their purpose:
+        // ------------------------------------------------------------------------
+        glDeleteVertexArrays(1, &VAO);
+        glDeleteBuffers(1, &VBO);
+    }
+    catch (const RuntimeError& error)
+    {
+        std::cout << error.what() << '\n';
+    }
+
+    return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(Event& report)
+{
+    if (report.IsKeyPressed(KeyboardKey::W))
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+
+    if (report.IsKeyPressed(KeyboardKey::S))
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+
+    if (report.IsKeyPressed(KeyboardKey::A))
+        camera.ProcessKeyboard(LEFT, deltaTime);
+
+    if (report.IsKeyPressed(KeyboardKey::D))
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(Event& report)
+{
+    if (report.Type == IsMouseMove)
+    {
+        double xposIn = report.Mouse.PosX;
+        double yposIn = report.Mouse.PosY;
+
+        float xpos = static_cast<float>(xposIn);
+        float ypos = static_cast<float>(yposIn);
+
+        if (firstMouse)
+        {
+            lastX = xpos;
+            lastY = ypos;
+            firstMouse = false;
+        }
+
+        float xoffset = xpos - lastX;
+        float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+        lastX = xpos;
+        lastY = ypos;
+
+        camera.ProcessMouseMovement(xoffset, yoffset);
+    }
+}
--- dependencies_download.bat
+++ dependencies_download.bat
@@ -1,3 +1,4 @@
 git clone https://github.com/JordanCpp/Lib-LDL.git dependencies/LDL
+git clone https://github.com/g-truc/glm.git        dependencies/glm
 
 pause
